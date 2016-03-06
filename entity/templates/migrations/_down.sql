e6a3ee5ff464e0aeabb64d0bd2554f0b9c81388f
Andrew Straw <strawman@astraw.com>
2016-03-06 19:53:26 +0100

    delete everything but license and nickel-diesel middleware

--- entity/templates/migrations/_down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE <%= pluralize(name) %>;
--- entity/templates/migrations/_up.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE <%= pluralize(name) %> (
-    <% _.each(attrs, function (attr) { %>
-    <%= attr.attrName %> <%= attr.attrSqlType %><% if (attr.attrType == 'Enum' || attr.attrType == 'String') { if (attr.maxLength) { %>(<%= attr.maxLength %>)<% } else { %>(255)<% }} %><% if (attr.required) { %> NOT NULL<% }; %>, <%}); %>
-    id SERIAL
-);
